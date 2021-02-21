7db80ce79f43f0928d1e54b0e731a17de4ce7624
dragongg44 <69012112+dragongg44@users.noreply.github.com>
2021-02-21 18:04:20 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+    le site a été créer pour un challenge graven hôpital
+
+    le site a été créer par oxydis et dragon_killl
---
+++ menu.js
@@ -0,0 +1,16 @@
+
+
+const showMenu = (toggleId, navbarId, bodyId)=>{
+    const toggle = document.getElementById(toggleId),
+    navbar = document.getElementById(navbarId),
+    bodypadding = document.getElementById(bodyId)
+  
+    if(toggle && navbar){
+      toggle.addEventListener('click', ()=>{
+        navbar.classList.toggle('expander')
+  
+        bodypadding.classList.toggle('body-pd')
+      })
+    }
+  }
+  showMenu('nav-toggle','navbar','body-pd')
